e5705dbbc80d36186e857de82c40173281a4a467
Weronika Adamczyk <weronika.mroziak@gmail.com>
2023-02-19 00:37:42 +0100

    Add final text shops.py

--- shops.py
+++ shops.py
@@ -6,3 +6,4 @@ print("Lista zakupów")
 
 for shop in list:
     print(f"Ide do {shop.capitalize()}", "i kupuję tam:", (list[shop]))
+print(f'W sumie kupuję :{len(list["piekarnia"])+len(list["warzywniak"])} produktów')
