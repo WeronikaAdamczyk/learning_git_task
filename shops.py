dbd857bbdf1a6a7a885ce4da7f9c970106491606
Weronika Adamczyk <weronika.mroziak@gmail.com>
2023-02-28 01:02:26 +0100

    modified shops.py

--- shops.py
+++ shops.py
@@ -7,3 +7,4 @@ print("Lista zakupów")
 for shop in list:
     print(f"Ide do {shop.capitalize()}", "i kupuję tam:", (list[shop]))
 print(f'W sumie kupuję :{len(list["piekarnia"])+len(list["warzywniak"])} produktów')
+print("mam 100zł w portfelu")
